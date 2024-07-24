445df777977800fd3fb72597e4206a9355beb548
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-07-24 17:26:50 -0600

    .

--- TEST 3 LUX/FORMS/Principal actividades.Designer.cs
+++ TEST 3 LUX/FORMS/Principal actividades.Designer.cs
@@ -34,11 +34,16 @@
             this.pictureBoxPictogramas = new System.Windows.Forms.PictureBox();
             this.pictureboxRetroceder = new System.Windows.Forms.PictureBox();
             this.panelActividadesBotones = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.panelSuperior.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPictogramas)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureboxRetroceder)).BeginInit();
+            this.panelActividadesBotones.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // panelSuperior
@@ -59,7 +64,7 @@
             // 
             this.pictureBox3.BackgroundImage = global::TEST_3_LUX.Properties.Resources.TecladoActividades;
             this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(351, 21);
+            this.pictureBox3.Location = new System.Drawing.Point(351, 20);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(104, 108);
             this.pictureBox3.TabIndex = 3;
@@ -69,7 +74,7 @@
             // 
             this.pictureBox2.BackgroundImage = global::TEST_3_LUX.Properties.Resources.DibujarActividades;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(242, 21);
+            this.pictureBox2.Location = new System.Drawing.Point(242, 20);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(104, 108);
             this.pictureBox2.TabIndex = 2;
@@ -79,7 +84,7 @@
             // 
             this.pictureBoxPictogramas.BackgroundImage = global::TEST_3_LUX.Properties.Resources.PictogramasActividades;
             this.pictureBoxPictogramas.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBoxPictogramas.Location = new System.Drawing.Point(134, 21);
+            this.pictureBoxPictogramas.Location = new System.Drawing.Point(134, 20);
             this.pictureBoxPictogramas.Name = "pictureBoxPictogramas";
             this.pictureBoxPictogramas.Size = new System.Drawing.Size(104, 108);
             this.pictureBoxPictogramas.TabIndex = 1;
@@ -101,17 +106,39 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelActividadesBotones.BackColor = System.Drawing.Color.Transparent;
+            this.panelActividadesBotones.Controls.Add(this.pictureBox4);
+            this.panelActividadesBotones.Controls.Add(this.pictureBox1);
             this.panelActividadesBotones.Location = new System.Drawing.Point(-6, 285);
             this.panelActividadesBotones.Name = "panelActividadesBotones";
             this.panelActividadesBotones.Size = new System.Drawing.Size(1306, 296);
             this.panelActividadesBotones.TabIndex = 1;
             this.panelActividadesBotones.Paint += new System.Windows.Forms.PaintEventHandler(this.panelActividadesBotones_Paint);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::TEST_3_LUX.Properties.Resources.DibujarActividades;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(160, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 149);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::TEST_3_LUX.Properties.Resources.DibujarActividades;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(341, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(301, 274);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
             // Principal_actividades
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(182)))), ((int)(((byte)(255)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(15)))), ((int)(((byte)(34)))));
             this.BackgroundImage = global::TEST_3_LUX.Properties.Resources.FondodeActividades;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1300, 725);
@@ -128,6 +155,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPictogramas)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureboxRetroceder)).EndInit();
+            this.panelActividadesBotones.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -140,5 +170,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBoxPictogramas;
         private System.Windows.Forms.Panel panelActividadesBotones;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
