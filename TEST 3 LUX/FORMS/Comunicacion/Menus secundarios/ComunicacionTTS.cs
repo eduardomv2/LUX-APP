a2d73cb0cf726de4ce428bedfbb769ccdd629502
Axel-18hrs <Axel.18hrs@gmail.com>
2024-07-24 02:07:26 -0600

    Inclusion de nuevos sub-apartados para la seccion de "Comunicacion"

---
+++ TEST 3 LUX/FORMS/Comunicacion/Menus secundarios/ComunicacionTTS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace TEST_3_LUX.FORMS.Comunicacion.Menus_secundarios
+{
+    public partial class ComunicacionTTS : Form
+    {
+        private SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public ComunicacionTTS()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            synthesizer.SpeakAsync(textBox1.Text);
+        }
+    }
+}
