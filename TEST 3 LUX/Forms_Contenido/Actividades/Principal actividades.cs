9ef1852832f869b02b0b42b2854d7776ddf7310a
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-07-29 04:46:09 -0600

    Confirmar algunos cambios (Animaciones) para sincronizar rama antes de hacer mas cambios

--- TEST 3 LUX/Forms_Contenido/Actividades/Principal actividades.cs
+++ TEST 3 LUX/Forms_Contenido/Actividades/Principal actividades.cs
@@ -10,25 +10,26 @@ using System.Windows.Forms;
 
 namespace TEST_3_LUX.FORMS
 {
-
-
-
     public partial class Principal_actividades : Form
     {
-
-        private Pictogramas_Actividades pictogramasActividades;
-
+    
+        string Transicion;
 
 
         public Principal_actividades()
         {
             InitializeComponent();
-            
+            tableLayoutPanel1.Visible = false;  
 
         }
 
         private void Principal_actividades_Load(object sender, EventArgs e)
         {
+            //button3.Image = Image.FromFile("C:\\Users\\eduar\\source\\repos\\LUX-APP\\TEST 3 LUX\\Forms_Contenido\\Actividades\\GIF\\011535.gif");
+
+            this.Top = this.Top + 15;
+            Transicion = "FadeIn";
+            tmTransicion.Start();
 
         }
 
@@ -45,9 +46,9 @@ namespace TEST_3_LUX.FORMS
        
         private void btnRetroceder_Click(object sender, EventArgs e)
         {
-            Principal principal = new Principal();
-            principal.Show();
-            this.Hide();
+            Transicion = "FadeOut";
+            tmTransicion.Start();
+
         }
 
         private void btnPictogramas_Click(object sender, EventArgs e)
@@ -64,9 +65,9 @@ namespace TEST_3_LUX.FORMS
 
         private void btnDibujar_Click(object sender, EventArgs e)
         {
-            DibujarActividades dibujarActividades = new DibujarActividades();
-            dibujarActividades.Show();
-            this.Hide();
+            //tableLayoutPanel1
+            tableLayoutPanel1.Visible = !tableLayoutPanel1.Visible;
+         
         }
 
         private void btnTeclado_Click(object sender, EventArgs e)
@@ -94,11 +95,69 @@ namespace TEST_3_LUX.FORMS
             this.Hide();
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
+       
+
+        
+
+        private void button3_Click_1(object sender, EventArgs e)
         {
-            //hacer transparente el panel
-            panel2.BackColor = Color.FromArgb(100, 0, 0, 0);
-            
+
+        }
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            DibujarActividades dibujarActividades = new DibujarActividades();
+            dibujarActividades.Show();
+            this.Hide();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tmTransicion_Tick(object sender, EventArgs e)
+        {     
+            if (Transicion == "FadeIn")
+            {
+                if (this.Opacity == 1)
+                {
+                    tmTransicion.Stop();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity + .15;
+                    this.Top = this.Top - 3;
+                }
+            }
+            else if (Transicion == "FadeOut")
+            {
+                if (this.Opacity == 0)
+                {
+                    Principal principal = new Principal();
+                    principal.Show();
+                    tmTransicion.Stop();
+                    this.Dispose();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity - .15;
+                    this.Top = this.Top + 3;
+                }
+            }
+            else if (Transicion == "FadeOutExit")
+            {
+                if (this.Opacity == 0)
+                {
+                    tmTransicion.Stop();
+                    Application.Exit();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity - .10;
+                    this.Left = this.Left + 3;
+                }
+            }
         }
     }
 }
--- TEST 3 LUX/Forms_Contenido/Principal/Principal.cs
+++ TEST 3 LUX/Forms_Contenido/Principal/Principal.cs
@@ -7,6 +7,8 @@ namespace TEST_3_LUX
 {
     public partial class Principal : Form
     {
+        string Transicion;
+
         public Principal()
         {
             InitializeComponent();
@@ -15,7 +17,12 @@ namespace TEST_3_LUX
   
         private void Principal_Load(object sender, EventArgs e)
         {
-            
+            Transicion = "FadeIn";
+
+            this.Top = this.Top + 15;
+
+
+            tmTransicion.Start();
         }
 
 
@@ -42,7 +49,8 @@ namespace TEST_3_LUX
         private void btnCerrar_Click(object sender, EventArgs e)
         {
             //Cerrar ventana
-            Application.Exit();
+            Transicion = "FadeOutExit";
+            tmTransicion.Start();
         }
         #endregion
 
@@ -53,6 +61,11 @@ namespace TEST_3_LUX
             Principal_actividades act = new Principal_actividades();
             act.Show();
             this.Hide();
+
+            Transicion = "FadeOut";
+            tmTransicion.Start();
+
+
         }
 
         private void btnComunicacion_Click_1(object sender, EventArgs e)
@@ -79,5 +92,47 @@ namespace TEST_3_LUX
             System.Diagnostics.Process.Start("https://github.com/eduardomv2/LUX-APP");
         }
         #endregion
+
+        private void tmTransicion_Tick(object sender, EventArgs e)
+        {
+            if (Transicion == "FadeOut")
+            {
+                if (this.Opacity == 0)
+                {
+                    tmTransicion.Stop();                
+                    this.Hide();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity - .15;
+                    this.Top = this.Top + 3;   
+                }
+            }
+            else if (Transicion == "FadeIn")
+            {
+                if (this.Opacity == 1)
+                {
+                    tmTransicion.Stop();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity + .15;
+                    this.Top = this.Top - 3;
+                }
+            }
+            else if (Transicion == "FadeOutExit")
+            {
+                if (this.Opacity == 0)
+                {
+                    tmTransicion.Stop();
+                    Application.Exit();
+                }
+                else
+                {
+                    this.Opacity = this.Opacity - .10;
+                    this.Left = this.Left + 3;
+                }
+            }
+        }
     }
 }
