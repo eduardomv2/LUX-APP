4c400951eff8f23827cf802c5c340ad1bd1008d5
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-08-15 11:53:27 -0600

    Opcion correcta

--- TEST 3 LUX/Forms_Contenido/Actividades/Secciones/Pictogramas/Opcion_Correcta/Opcion_Correcta.Designer.cs
+++ TEST 3 LUX/Forms_Contenido/Actividades/Secciones/Pictogramas/Opcion_Correcta/Opcion_Correcta.Designer.cs
@@ -28,12 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAnimal = new System.Windows.Forms.Button();
+            this.btnComida = new System.Windows.Forms.Button();
+            this.btnPersona = new System.Windows.Forms.Button();
+            this.btnObjeto = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Location = new System.Drawing.Point(314, 66);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(723, 330);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Transparent;
+            this.panel3.BackgroundImage = global::TEST_3_LUX.Properties.Resources.Diseño_sin_título___2024_08_15T052707_487;
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(26, 17);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(242, 122);
+            this.panel3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(18, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 74);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImage = global::TEST_3_LUX.Properties.Resources.Agregar_un_subtítulo__3_;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.btnAnimal);
+            this.panel1.Controls.Add(this.btnComida);
+            this.panel1.Controls.Add(this.btnPersona);
+            this.panel1.Controls.Add(this.btnObjeto);
+            this.panel1.Location = new System.Drawing.Point(233, 412);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(899, 314);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btnAnimal
+            // 
+            this.btnAnimal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAnimal.FlatAppearance.BorderSize = 0;
+            this.btnAnimal.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnAnimal.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnAnimal.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAnimal.Location = new System.Drawing.Point(21, 23);
+            this.btnAnimal.Name = "btnAnimal";
+            this.btnAnimal.Size = new System.Drawing.Size(406, 109);
+            this.btnAnimal.TabIndex = 3;
+            this.btnAnimal.UseVisualStyleBackColor = true;
+            this.btnAnimal.Click += new System.EventHandler(this.btnAnimal_Click);
+            // 
+            // btnComida
+            // 
+            this.btnComida.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnComida.FlatAppearance.BorderSize = 0;
+            this.btnComida.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnComida.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnComida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnComida.Location = new System.Drawing.Point(21, 177);
+            this.btnComida.Name = "btnComida";
+            this.btnComida.Size = new System.Drawing.Size(406, 109);
+            this.btnComida.TabIndex = 2;
+            this.btnComida.UseVisualStyleBackColor = true;
+            this.btnComida.Click += new System.EventHandler(this.btnComida_Click);
+            // 
+            // btnPersona
+            // 
+            this.btnPersona.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPersona.FlatAppearance.BorderSize = 0;
+            this.btnPersona.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPersona.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPersona.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPersona.Location = new System.Drawing.Point(467, 177);
+            this.btnPersona.Name = "btnPersona";
+            this.btnPersona.Size = new System.Drawing.Size(406, 109);
+            this.btnPersona.TabIndex = 1;
+            this.btnPersona.UseVisualStyleBackColor = true;
+            this.btnPersona.Click += new System.EventHandler(this.btnPersona_Click);
+            // 
+            // btnObjeto
+            // 
+            this.btnObjeto.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnObjeto.FlatAppearance.BorderSize = 0;
+            this.btnObjeto.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnObjeto.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnObjeto.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnObjeto.Location = new System.Drawing.Point(467, 23);
+            this.btnObjeto.Name = "btnObjeto";
+            this.btnObjeto.Size = new System.Drawing.Size(406, 109);
+            this.btnObjeto.TabIndex = 0;
+            this.btnObjeto.UseVisualStyleBackColor = true;
+            this.btnObjeto.Click += new System.EventHandler(this.btnObjeto_Click);
+            // 
+            // Opcion_Correcta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1300, 725);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Opcion_Correcta";
             this.Text = "Opcion_Correcta";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Opcion_Correcta_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAnimal;
+        private System.Windows.Forms.Button btnComida;
+        private System.Windows.Forms.Button btnPersona;
+        private System.Windows.Forms.Button btnObjeto;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
     }
 }
