cbe1c4ff476226e48d67d0daacbea483628e594e
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2024-08-27 23:25:12 -0600

    Se eliminaron botones

--- TEST 3 LUX/Forms_Contenido/Principal/Principal.Designer.cs
+++ TEST 3 LUX/Forms_Contenido/Principal/Principal.Designer.cs
@@ -267,6 +267,7 @@
             this.button3.Size = new System.Drawing.Size(250, 259);
             this.button3.TabIndex = 7;
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // tmTransicion
             // 
